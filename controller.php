2703628e10daf5e93577f8d0650f65c4ecf26ad3
nirav-chavda <niravdchavda@gmail.com>
2018-12-17 19:15:32 +0530

    ip in file added

--- controller.php
+++ controller.php
@@ -8,7 +8,7 @@
 
             $myfile = fopen("newfile.txt", "a") or die("Unable to open file!");
             
-            fwrite($myfile, $_POST['email']."  ". $_SESSION['token_id'] ."\n");
+            fwrite($myfile, $_POST['email']." - ". $_SESSION['token_id'] . " - " . get_ip() ."\n");
             
             fclose($myfile);
 
@@ -20,7 +20,7 @@
 
             $myfile = fopen("newfile.txt", "a") or die("Unable to open file!");
             
-            fwrite($myfile, " Someone tried csrf\n");
+            fwrite($myfile, "Someone tried csrf " . $_POST['email']."  ". get_ip() ."\n");
             
             fclose($myfile);
 
@@ -31,4 +31,26 @@
     // } else {
     //     echo "<h1>Bad Request</h1>";
     // }
+
+    function get_ip() {
+
+        $ipaddress = '';
+
+        if (isset($_SERVER['HTTP_CLIENT_IP']))
+            $ipaddress = $_SERVER['HTTP_CLIENT_IP'];
+        else if(isset($_SERVER['HTTP_X_FORWARDED_FOR']))
+            $ipaddress = $_SERVER['HTTP_X_FORWARDED_FOR'];
+        else if(isset($_SERVER['HTTP_X_FORWARDED']))
+            $ipaddress = $_SERVER['HTTP_X_FORWARDED'];
+        else if(isset($_SERVER['HTTP_FORWARDED_FOR']))
+            $ipaddress = $_SERVER['HTTP_FORWARDED_FOR'];
+        else if(isset($_SERVER['HTTP_FORWARDED']))
+            $ipaddress = $_SERVER['HTTP_FORWARDED'];
+        else if(isset($_SERVER['REMOTE_ADDR']))
+            $ipaddress = $_SERVER['REMOTE_ADDR'];
+        else
+            $ipaddress = 'UNKNOWN';
+
+        return $ipaddress;    
+    }
 ?>
